399f11980e3a8a424f793542d81e068c5ec4ba9f
ApostolosIrakleous <10611069@mydbs.ie>
2023-07-29 01:48:38 +0100

    adding products and css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Button } from "bootstrap";
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -6,7 +9,25 @@ function App() {
        
       </header>
       <main>
-        list products
+        <h1>Featured products</h1>
+        <div className="products"> 
+        {
+          data.products.map(product => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name}/>
+            </a>
+            <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+            </a>
+            <p>
+              <strong>${product.price}</strong></p>
+            <button>Add to cart</button>
+            </div>
+          </div>))          
+        }
+        </div>
 
 
 
