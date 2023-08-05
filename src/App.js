9af9e3ebdf03bf0cd576e37520f2f0d16f5d939b
ApostolosIrakleous <10611069@mydbs.ie>
2023-08-05 18:51:07 +0100

    adding header and footer for both Ba/fr with boostrap (intalled packages)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import { BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap'; //named import
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header >                                        
-        <Link to = "/">ca</Link>
+    <div  className="d-flex flex-column site-container">
+      <header > 
+      <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Greek Gifts</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>                                       
+        
        
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen />} />
           <Route path ="/" element={<HomeScreen/>} />
         </Routes>
+        </Container>
 
       </main>
+      <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+
     </div>
     </BrowserRouter>
   );
