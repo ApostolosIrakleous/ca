d1412c847da2e6b8bc046a3e8fde8cecab9a50d8
ApostolosIrakleous <10611069@mydbs.ie>
2023-08-07 00:34:26 +0100

    created react add to cart products part1

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
+import { StoreProvider } from './Store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StoreProvider>
     <HelmetProvider>
-    <App />
+      <App />
     </HelmetProvider>
-  </React.StrictMode>
+    </StoreProvider>
+  </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
