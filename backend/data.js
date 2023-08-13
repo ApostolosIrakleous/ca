020db826fad82d05161d73fab40aa284d67bedb7
ApostolosIrakleous <10611069@mydbs.ie>
2023-08-13 17:31:07 +0100

    created sample user to database(routes,models)installed bcryptjs

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Apostolos',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Ornela',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
     products: [
       {
         //_id:'1',
