e24b624f851086594a6a78097f22285ec62e88cd
ApostolosIrakleous <10611069@mydbs.ie>
2023-08-19 19:56:43 +0100

    edit product as admin

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 import DashboardScreen from './screens/DashboardScreen';
 import AdminRoute from './components/AdminRoute';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 function App() {
   const { state ,dispatch: ctxDispatch} = useContext(Store);
@@ -172,6 +173,7 @@ function App() {
           {/* Admin Routes */}
           <Route path="/admin/dashboard" element={<AdminRoute><DashboardScreen /></AdminRoute>}></Route>
           <Route path="/admin/products" element={<AdminRoute><ProductListScreen /></AdminRoute>}></Route>
+          <Route path="/admin/product/:id" element={<AdminRoute><ProductEditScreen /></AdminRoute>}></Route>
           <Route path ="/" element={<HomeScreen/>} />
         </Routes>
         </Container>
