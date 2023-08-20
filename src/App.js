cdd86836f42de303311613768404e80e66d7e9b7
ApostolosIrakleous <10611069@mydbs.ie>
2023-08-20 20:08:47 +0100

    Edit user as admin: delete ,uptade

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 function App() {
   const { state ,dispatch: ctxDispatch} = useContext(Store);
@@ -178,6 +179,7 @@ function App() {
           <Route path="/admin/users" element={<AdminRoute><UserListScreen /></AdminRoute>}></Route>
           <Route path="/admin/products" element={<AdminRoute><ProductListScreen /></AdminRoute>}></Route>
           <Route path="/admin/product/:id" element={<AdminRoute><ProductEditScreen /></AdminRoute>}></Route>
+          <Route path="/admin/user/:id" element={<AdminRoute><UserEditScreen /></AdminRoute>}></Route>
           <Route path ="/" element={<HomeScreen/>} />
         </Routes>
         </Container>
